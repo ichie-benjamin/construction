ed7f2e7d4f2f0ccfe205fda2a411d632aab41039
benny <benny@benny-optiplex-7060>
2022-05-25 13:44:51 +0100

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -323,5 +323,22 @@
 <script src="/js/appear.js"></script>
 <script src="/js/script.js"></script>
 
+
+
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/628e23c47b967b1179912a53/1g3tjneh5';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
+
 </body>
 </html>
